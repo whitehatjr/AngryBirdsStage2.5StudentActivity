7510a4e70f02d82653bbf1a3b36ba33acf1e705e
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2019-07-12 00:15:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,11 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
+var backgroundImg;
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -33,7 +38,7 @@ function setup(){
 }
 
 function draw(){
-    background(0);
+    background(backgroundImg);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
