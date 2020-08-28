3f60d63c91ef82efd7bf834a4a58c70f42bb2657
ShyamBhalodiya <65011653+ShyamBhalodiya@users.noreply.github.com>
2020-08-28 18:14:30 +0530

    Removing excess of console.log()'s
    There are three consoles in the file named sketch.js which may create problem for users

--- sketch.js
+++ sketch.js
@@ -41,9 +41,6 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
